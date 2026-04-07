b8a7fe460ea26284b57e8c5796eeae005fa004c3
AnirudhCS-SASTRA2020 <124159008@sastra.ac.in>
2026-04-07 17:46:45 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy
 
 # Extract
 df = pd.read_csv("orders.csv")
 
 # Transform
 df = df[df["amount"] > 100]
+
+# Hi There (IN local, it was Hello there, here it is Hi There. This will cause a merge conflict)
 
 # Load
 df.to_csv("cleaned_orders.csv", index=False)
